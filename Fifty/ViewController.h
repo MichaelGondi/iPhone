d214d1b98e7bde233613612186288426bc8025c3
Michael Gondi <Michael@Michaels-MacBook-Pro.local>
2012-07-19 10:40:49 -0700

    Initial Commit

---
+++ Fifty/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fifty
+//
+//  Created by Michael  Gondi on 7/19/12.
+//  Copyright (c) 2012 UC Merced. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
